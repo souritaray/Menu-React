ae2b1d40e29319b547d3a8c4ea95ec5386f701f3
souritaray <100030958+souritaray@users.noreply.github.com>
2023-07-06 09:26:05 +0530

    setup started

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 
 function App() {
   return (
-   
+   <>
+   <main>
+    <section className="menu section">
+      <div className = "title">
+      <h2>Our Menu</h2>
+      <div class="underline"></div>
+      </div>
+    </section>
+   </main>
+   </>
   );
 }
 
